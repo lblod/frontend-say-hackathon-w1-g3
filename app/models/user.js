d9978b8000174dc8d82a30de8cb75dc19866f560
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-09-12 21:21:40 +0200

    user fixup

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class UserModel extends Model {
   @attr firstName;
-  @attr lastName;
+  @attr familyName;
 
   @hasMany('account', { async: false, inverse: 'user' }) account;
 
