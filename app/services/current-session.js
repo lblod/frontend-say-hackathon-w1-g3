ef23187a7a0e99670456c158adbfc9bfda6c55f7
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-09-12 21:19:45 +0200

    fixup user loading

--- app/services/current-session.js
+++ app/services/current-session.js
@@ -11,25 +11,21 @@ export default class CurrentSessionService extends Service {
   @tracked groupClassification;
   @tracked _roles = [];
 
+  get isLoaded() {
+    return Boolean(this.account && this.user);
+  }
+
   async load() {
     if (this.session.isAuthenticated) {
       let accountId =
         this.session.data.authenticated.relationships.account.data.id;
 
       this.account = await this.store.findRecord('account', accountId, {
-        include: 'gebruiker.bestuurseenheden.classificatie',
+        include: 'user',
       });
 
       this.user = this.account.user;
       this._roles = this.session.data.authenticated.data.attributes.roles;
-
-      // We need to do an extra API call here because ACM/IDM users don't seem to have a "bestuurseenheden" relationship in the DB.
-      // By fetching the record directly we bypass that issue
-      const groupId =
-        this.session.data.authenticated.relationships.group.data.id;
-      this.group = await this.store.findRecord('administrative-unit', groupId, {
-        reload: true,
-      });
     }
   }
 
