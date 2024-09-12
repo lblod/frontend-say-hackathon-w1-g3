bed00bdc32c29fdc92454ab2ae017a8afcc9a287
Ruben Bottu <ruben.bottu@qmino.com>
2024-09-12 18:34:10 +0200

    Add straatnaam input field to page 1

--- app/components/aanduidingsobjecten.js
+++ app/components/aanduidingsobjecten.js
@@ -4,15 +4,39 @@ import { tracked } from '@glimmer/tracking';
 export default class Aanduidingsobjecten extends Component {
   @tracked aanduidingsobjecten = [];
 
-  constructor() {
-    super(...arguments);
-  }
+  onNameInputChange = async (event) => {
+    const input = event.target.value;
+    if (input === '') {
+      this.aanduidingsobjecten = [];
+      return;
+    }
 
-  onGemeenteChange = async (event) => {
     const url = new URL(
       'https://inventaris.onroerenderfgoed.be/aanduidingsobjecten',
     );
-    url.searchParams.append('tekst', event.target.value);
+    url.searchParams.append('tekst', input);
+
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    // TODO add error handling
+    const data = await response.json();
+    this.aanduidingsobjecten = data;
+  };
+
+  onStraatChange = async (event) => {
+    const input = event.target.value;
+    if (input === '') {
+      this.aanduidingsobjecten = [];
+      return;
+    }
+
+    const url = new URL(
+      'https://inventaris.onroerenderfgoed.be/aanduidingsobjecten',
+    );
+    url.searchParams.append('straat', input);
 
     const response = await fetch(url, {
       headers: {
